70e0cd5992f858639d51f952ece84f3a3917a762
maximethiry <mtwww@proton.me>
2024-08-18 14:57:15 +0200

    cleaned services.php

--- config/services.php
+++ config/services.php
@@ -10,10 +10,10 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters->set('app.environment', '%kernel.environment%');
     $parameters->set('app.project_directory', '%kernel.project_dir%');
     $parameters->set('app.public_directory', '%kernel.project_dir%/public');
-    $parameters->set('app.upload.relative_directory', '/user_uploads');
-    $parameters->set('app.upload.bytes_max_filesize', 50000000);
+    $parameters->set('app.upload_directory', '%kernel.project_dir%/user_uploads');
     $parameters->set('app.converted_upload_directory', '%kernel.project_dir%/user_converted_uploads');
     $parameters->set('app.thumbnail_directory', '%kernel.project_dir%/public/snap');
+    $parameters->set('app.upload.bytes_max_filesize', 50000000);
 
     $services = $containerConfigurator->services();
 
--- src/Infrastructure/Domain/Impl/Snap/FileStorage/LocalFileStorage.php
+++ src/Infrastructure/Domain/Impl/Snap/FileStorage/LocalFileStorage.php
@@ -16,8 +16,7 @@ use Symfony\Component\Uid\Uuid;
 final readonly class LocalFileStorage implements FileStorageInterface
 {
     public function __construct(
-        #[Autowire(param: 'app.project_directory')] private string $projectDirectory,
-        #[Autowire(param: 'app.upload.relative_directory')] private string $uploadRelativeDirectory,
+        #[Autowire(param: 'app.upload_directory')] private string $uploadDirectory,
         #[Autowire(param: 'app.upload.bytes_max_filesize')] private int $uploadBytesMaxFilesize,
         private FormatConverterService $formatConverterService,
         private Filesystem $filesystem = new Filesystem()
@@ -31,12 +30,7 @@ final readonly class LocalFileStorage implements FileStorageInterface
 
     public function store(Uuid $snapId, Uuid $snapUserId, FileMetadata $fileMetadata): void
     {
-        $userPersonalUploadDirectory = sprintf(
-            '%s%s/%s/',
-            $this->projectDirectory,
-            $this->uploadRelativeDirectory,
-            $snapUserId->toBase58()
-        );
+        $userPersonalUploadDirectory = sprintf('%s/%s', $this->uploadDirectory, $snapUserId->toBase58());
 
         if ($this->filesystem->exists($userPersonalUploadDirectory) === false) {
             $this->filesystem->mkdir($userPersonalUploadDirectory);
@@ -44,19 +38,13 @@ final readonly class LocalFileStorage implements FileStorageInterface
 
         $this->filesystem->copy(
             $fileMetadata->getPath(),
-            $userPersonalUploadDirectory . $snapId->toBase58()
+            sprintf('%s/%s', $userPersonalUploadDirectory, $snapId->toBase58())
         );
     }
 
     public function delete(Snap $snap): void
     {
-        $filePath = sprintf(
-            '%s%s/%s/%s',
-            $this->projectDirectory,
-            $this->uploadRelativeDirectory,
-            $snap->getUserId()->toBase58(),
-            $snap->getId()->toBase58()
-        );
+        $filePath = sprintf('%s/%s/%s', $this->uploadDirectory, $snap->getUserId()->toBase58(), $snap->getId()->toBase58());
 
         $this->filesystem->remove($filePath);
         $this->formatConverterService->deleteConvertedFiles($snap);
@@ -64,13 +52,7 @@ final readonly class LocalFileStorage implements FileStorageInterface
 
     public function get(Uuid $snapId, Uuid $snapUserId): ?File
     {
-        $filePath = sprintf(
-            '%s%s/%s/%s',
-            $this->projectDirectory,
-            $this->uploadRelativeDirectory,
-            $snapUserId->toBase58(),
-            $snapId->toBase58()
-        );
+        $filePath = sprintf('%s/%s/%s', $this->uploadDirectory, $snapUserId->toBase58(), $snapId->toBase58());
 
         if ($this->filesystem->exists($filePath) === false) {
             return null;
