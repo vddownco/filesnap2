5f2fee4e58c98e81ffc1f7e20ee85bbe3a06a70d
maximethiry <mtwww@proton.me>
2024-10-11 23:19:27 +0200

    updated example .env && cs fix

--- config/packages/messenger.php
+++ config/packages/messenger.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use App\Infrastructure\Symfony\Message\ConversionMessage;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
 use function Symfony\Component\DependencyInjection\Loader\Configurator\env;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
